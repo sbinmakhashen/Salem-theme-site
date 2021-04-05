16c8cce6002616500cb01e46c5469bc19a7f9e44
Salem <nlsabm@yahoo.com>
2021-04-05 18:46:17 -0400

    van links bugs

--- app.js
+++ app.js
@@ -10,3 +10,33 @@ window.onscroll = () => {
         ul.className = 'ul-bg'.remove;
     }
 }
+
+// highlighting nav links on scroll
+// getting the sections
+const sections = document.querySelectorAll('section');
+// get the lis from nav tag
+const ulLis = document.querySelectorAll('nav ul li');
+// console.log(ulLis);
+// event listener
+window.addEventListener('scroll', () => {
+    // looping through the sections
+    // init a var
+    let currentPage = '';
+    sections.forEach(pageSection => {
+        // gettig the top of page and height
+        const pageSecTop = pageSection.offsetTop;
+        const pageSecheight = pageSection.clientHeight;
+        if (pageYOffset >= pageSecTop) {
+            currentPage = pageSection.getAttribute('id');
+        }
+    });
+    // looping trough nav Lis 
+    ulLis.forEach(li => {
+        li.classList.remove('active');
+        // li.getElementsByTagName('a').remove;
+        if (li.classList.contains(currentPage)) {
+            li.classList.add('active')
+            // li.getElementsByTagName('a');
+        }
+    });
+});
