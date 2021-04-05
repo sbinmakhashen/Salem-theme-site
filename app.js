327c04f6556674a7877a9d08922db1452461d5dc
Salem Binmakhashen <sbinmakhashen@student.cscc.edu>
2021-04-04 23:56:33 -0400

    onscroll with arrow function

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onscroll = () => {
+    const navbar = document.querySelector('#nav');
+    if (this.scrollY <= 100) {
+        navbar.classList = '';
+    } else {
+        navbar.className = 
+    }
+}
