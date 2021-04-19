f7f74d2e146b9df3ebe88bb73231a3d97fac4f52
Salem Binmakhashen <sbinmakhashen@student.cscc.edu>
2021-04-19 19:43:39 -0400

    scroll es6

--- app.js
+++ app.js
@@ -44,25 +44,25 @@ function typing() {
 typing();
 // highlighting nav links on scroll
 
-// addEventListener('scroll', () => {
-//     // init nav links
-//     let navLinks = document.querySelectorAll('nav ul li a');
-//     let topPage = scrollY;
-//     // looping through the nav links
-//     navLinks.forEach(links => {
-//         // getting the sections by their has/#
-//         let sections = document.querySelector(links.hash);
-//         // console.log(sections);
-//         if (sections.offsetTop <= topPage
-//             && sections.offsetTop + sections.offsetHeight > topPage) {
-//             links.classList.add('active')
-//         } else {
-//             links.classList.remove('active')
-//         }
-//     });
-// });
+addEventListener('scroll', () => {
+    // init nav links
+    let navLinks = document.querySelectorAll('nav ul li a');
+    let topPage = scrollY;
+    // looping through the nav links
+    navLinks.forEach(links => {
+        // getting the sections by their has/#
+        let sections = document.querySelector(links.hash);
+        // console.log(sections);
+        if (sections.offsetTop <= topPage
+            && sections.offsetTop + sections.offsetHeight > topPage) {
+            links.classList.add('active')
+        } else {
+            links.classList.remove('active')
+        }
+    });
+});
 
-// // filter projects
+// filter projects
 
 // // get project's li's
 // const portLi = document.querySelectorAll('.port-menu ul li');
