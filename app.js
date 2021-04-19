69de00fb3d64e102530db52ab6d1d8c7f0dcd355
Salem Binmakhashen <sbinmakhashen@student.cscc.edu>
2021-04-19 19:14:19 -0400

    bg es6 bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-window.onscroll = () => {
-    const navbar = document.querySelector('.nav');
-    const ul = document.getElementsByTagName('ul')[0];
-    const pixTop = window.scrollY;
+window.onscroll = function () {
+    var navbar = document.querySelector('.nav');
+    var ul = document.getElementsByTagName('ul')[0];
+    var pixTop = window.scrollY;
     if (pixTop >= 50) {
         navbar.classList.add('bg-onscroll')
         ul.className = 'ul-bg';
